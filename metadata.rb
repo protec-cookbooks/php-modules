075f1e60a132c22297f7045640d39b59877e46da
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-06-23 22:35:26 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ description      "Installs/Configures php-modules"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+name "php-modules"
+
 %w{ debian ubuntu mint redhat centos amazon }.each do |os|
     supports os
 end
