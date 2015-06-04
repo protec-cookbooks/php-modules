65da013b298b3197f2f465e8425b899f0981f093
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2015-06-04 11:48:29 +0100

    Update to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+name "php-modules"
+maintainer       "Protec Innovations"
+maintainer_email "dev@protecinnovations.co.uk"
+license          "GPLv3"
 description      "Installs/Configures php-modules"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 
 %w{ debian ubuntu mint redhat centos amazon }.each do |os|
     supports os
